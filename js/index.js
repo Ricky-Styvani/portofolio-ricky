aa326428fe9b290f5a69ec6f500df9e64059e004
Ricky-Styvani <rickystyvani@gmail.com>
2021-03-05 16:46:54 +0700

    pindah js

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(window).on("load", function () {
+  $(".jumbotron img").addClass("imgMuncul");
+});
+
+$(window).scroll(function () {
+  var pScroll = $(this).scrollTop();
+  //jumbotro
+  $(".jumbotron img").css({
+    transform: "translate(0px, " + pScroll / 25 + "%)",
+  });
+  $(".jumbotron h1").css({
+    transform: "translate(0px, " + pScroll / 2 + "%)",
+  });
+  $(".jumbotron p").css({
+    transform: "translate(0px, " + pScroll / 1.3 + "%)",
+  });
+  //aboute.top - 90
+  if (pScroll > $(".aboute").offset().top - 250) {
+    $(".pKiri").addClass("pMuncul");
+    $(".pKanan").addClass("pMuncul");
+  }
+  //projects
+  if (pScroll > $(".projects").offset().top - 250) {
+    $(".projects .thumbnail").each(function (i) {
+      setTimeout(function () {
+        $(".projects .thumbnail").eq(i).addClass("muncul");
+      }, 300 * (i + 1));
+    });
+  }
+});
